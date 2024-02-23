657a3292b76c512932981fd10424c02db984a870
Evgeny Belov <e.belov@inbox.ru>
2024-02-23 20:43:20 +0300

    humanOrPeople was improved

--- src/main/kotlin/ru/netology/humanOrPeople.kt
+++ src/main/kotlin/ru/netology/humanOrPeople.kt
@@ -1,7 +1,7 @@
 package ru.netology
 
 fun main() {
-    val likes = 15781
-    var humanOrPeople = if (likes == 1 || (likes > 20 && likes - likes / 10 * 10 == 1)) "человеку" else "людям"
+    val likes = 511
+    var humanOrPeople = if (likes % 10 == 1 && likes % 100 !== 11) "человеку" else "людям"
     println("Понравилось $likes $humanOrPeople")
 }
